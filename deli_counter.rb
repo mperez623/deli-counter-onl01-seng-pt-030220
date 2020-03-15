2d0c73902bea827a4176ae5a212376c3d209f879
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-15 05:49:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,13 @@
 def line(names)
  if names.length == 0
    puts "The line is currently empty."
+ else 
+   pre_string = "The line is currently:"
+   string = ""
+  names.each_with_index do |n, i|
+    string += " #{i + 1}. #{n}"  
+  end
+  puts pre_string + string
  end
  
   
